28856acbd8214291dd8531a1a63df71c4f599cb2
vuvufelix <vuvufelix7@gmail.com>
2025-10-19 16:52:53 -0300

    Adicionado conexão com a google e facebook

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js"
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, FacebookAuthProvider} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js"
 
 const terms = document.querySelector("#terms")
 const btn = document.querySelector("#btn")
 const container = document.querySelector(".container")
+
+const name = document.querySelector("#first_name")
+const sobre_name = document.querySelector("#last_name")
+const email = document.querySelector("#email")
+const password =  document.querySelector("#password")
+
+const sms_first_name = document.querySelector("#sms_first_name")
+const sms_last_name = document.querySelector("#sms_last_name")
+const sms_email = document.querySelector("#sms_email")
+const sms_password = document.querySelector("#sms_password")
 
 const google_btn = document.querySelector("#google_btn")
 const apple_btn = document.querySelector("#apple_btn")
@@ -18,9 +28,11 @@ const firebaseConfig = {
     appId: "1:377829093201:web:e0c9d1f621b0a44252ed70"
 }
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 
-const auth = getAuth(app);
+const auth = getAuth(app)
+const provider = new GoogleAuthProvider()
+const providerFacebook = new FacebookAuthProvider();
 
 const createUser = async (email, password) => {
 
@@ -37,8 +49,37 @@ const createUser = async (email, password) => {
 
 }
 
+const loginWithGoogle = (autorization, provid) => {
+    signInWithPopup(autorization, provid)
+    .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result)
+        const user = result.user
+        message("Usuário logado com sucesso!", "greenyellow")
+        console.log(user)
+    }).catch((error) => {
+        const errorMessage = error.message
+        console.log(errorMessage)
+        message("Falha ao fazer login!", "red")
+  })
+}
+
+const loginWithFacebook = (autorization, provid) => {
+    signInWithPopup(autorization, provid)
+    .then((result) => {
+        const user = result.user
+        console.log(user)
+        message("Usuário logado com sucesso!", "greenyellow")
+    })
+    .catch((error) => {
+        const errorMessage = error.message
+        console.log(errorMessage)
+        message("Falha ao fazer login!", "red")
+    })
+}
+
+
 // Mensagem de cadastro
-const mensagem = (texto, cor) => {
+const message = (texto, cor) => {
     Toastify({
         text: texto,
         duration: 2000,
@@ -57,19 +98,7 @@ const mensagem = (texto, cor) => {
 btn.addEventListener("click", (e) => {
     e.preventDefault()
 
-    const sign_up = {
-        name: document.querySelector("#first_name").value,
-        sobre_name: document.querySelector("#last_name").value,
-        email: document.querySelector("#email").value,
-        password:  document.querySelector("#password").value
-    }
-
     const verifiquer = () => {
-
-        const sms_first_name = document.querySelector("#sms_first_name")
-        const sms_last_name = document.querySelector("#sms_last_name")
-        const sms_email = document.querySelector("#sms_email")
-        const sms_password = document.querySelector("#sms_password")
 
         sms_first_name.style.display = "block"
         sms_last_name.style.display = "block"
@@ -88,10 +117,10 @@ btn.addEventListener("click", (e) => {
         sms_password.style.marginTop = "5px"
         sms_password.style.fontSize = "0.7rem"
 
-        if(sign_up.name.length == 0) {
+        if(name.value == "") {
             sms_first_name.style.color = "red"
             sms_first_name.textContent = "Por favor preencha este campo"
-        } else if(sign_up.name.length > 0 && sign_up.name.length < 3) {
+        } else if(name.value != "" && name.value.length < 3) {
             sms_first_name.style.color = "red"
             sms_first_name.textContent = "O nome tem que ter no mínimo 3 caracteres"
         } else {
@@ -99,10 +128,10 @@ btn.addEventListener("click", (e) => {
             sms_first_name.textContent = "Nome válido"
         }
 
-        if(sign_up.sobre_name.length == 0) {
+        if(sobre_name.value.length == 0) {
             sms_last_name.style.color = "red"
             sms_last_name.textContent = "Por favor preencha este campo"
-        } else if(sign_up.sobre_name.length > 0 && sign_up.sobre_name.length < 3) {
+        } else if(sobre_name.value.length > 0 && sobre_name.value.length < 3) {
             sms_last_name.style.color = "red"
             sms_last_name.textContent = "O nome tem que ter no mínimo 3 caracteres"
         } else {
@@ -110,7 +139,7 @@ btn.addEventListener("click", (e) => {
             sms_last_name.textContent = "Nome válido"
         }
 
-        if(sign_up.email.length == 0) {
+        if(email.value.length == 0) {
             sms_email.style.color = "red"
             sms_email.textContent = "Por favor preencha este campo"
         } else {
@@ -118,10 +147,10 @@ btn.addEventListener("click", (e) => {
             sms_email.textContent = "Email válido"
         }
 
-        if(sign_up.password.length == 0) {
+        if(password.value.length == 0) {
             sms_password.style.color = "red"
             sms_password.textContent = "Por favor preencha este campo"
-        } else if(sign_up.password.length > 0 && sign_up.password.length < 6) {
+        } else if(password.value.length > 0 && password.value.length < 6) {
             sms_password.style.color = "red"
             sms_password.textContent = "A senha tem que ter no mínimo 6 caracteres"
         } else {
@@ -144,24 +173,29 @@ btn.addEventListener("click", (e) => {
     })
 
     if(sms_value && terms.checked) {
-        createUser(sign_up.email, sign_up.password)
-        mensagem("Cadastro realizado com sucesso", "greenyellow")
+        createUser(email.value, password.value)
+        message("Cadastro realizado com sucesso", "greenyellow")
         container.style.height = "35rem"
 
-        sms_password.textContent = ""
-        sms_email.textContent = ""
-        sms_last_name.textContent = ""
+        password.value = ""
+        email.value = ""
+        name.value = ""
+        sobre_name.value = ""
+
         sms_first_name.textContent = ""
+        sms_last_name.textContent = ""
+        sms_email.textContent = ""
+        sms_password.textContent = ""
     }
 
 })
 
 google_btn.addEventListener("click", (e) => {
     e.preventDefault()
-
+    loginWithGoogle(auth, provider)
 })
 
 apple_btn.addEventListener("click", (e) => {
     e.preventDefault()
-
+    loginWithFacebook(auth, providerFacebook)
 })
